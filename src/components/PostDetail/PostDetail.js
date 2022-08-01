2504c24b839705380b6ac60b91b394e24b25b642
HP <sreekanta.csd@gmail.com>
2022-08-01 23:59:48 +0600

    nested router added

--- src/components/PostDetail/PostDetail.js
+++ src/components/PostDetail/PostDetail.js
@@ -1,24 +1,25 @@
-import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 const PostDetail = () => {
-    const {postId} = useParams()
-    const [post, setPost] = useState({})
-   
-    
-   useEffect(()=>{
-    fetch(`https://jsonplaceholder.typicode.com/posts/${postId}`)
-    .then(res =>res.json())
-    .then(data => setPost(data))
-   },[postId])
+  const { postId } = useParams();
+  const [post, setPost] = useState({});
 
-    return (
-        <div>
-            <h1>This is post Detail:  {postId}</h1>
-            <p> {post.title}</p>
-            <p>{post.body}</p>
-        </div>
-    );
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/posts/${postId}`)
+      .then((res) => res.json())
+      .then((data) => setPost(data));
+  }, [postId]);
+
+  return (
+    <div className="flex justify-center  ">
+      <div className="border-4 border-blue-600 w-1/2 rounded-lg shadow-lg p-3 mt-4">
+        <h1>This is post Detail: {postId}</h1>
+        <p className="text-3xl font-bold text-pink-600"> {post.title}</p>
+        <p className="tex-xl font-bold">{post.body}</p>
+      </div>
+    </div>
+  );
 };
 
 export default PostDetail;
--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -3,6 +3,7 @@ import { Link, Outlet } from 'react-router-dom';
 
 const Posts = () => {
     const [posts, setPosts] = useState([])
+    console.log(posts)
     
 
     fetch('https://jsonplaceholder.typicode.com/posts')
@@ -11,12 +12,17 @@ const Posts = () => {
     return (
         <div>
             <h1>This is for Post</h1>
+
             {
                 posts.map(post => <Link 
                     key={post.id}
                     to= {`/posts/${post.id}`}
                     >{post.id}   </Link>)
             }
+            {
+                posts.map(post => <Link key={post.id} to={`posts/${post.id}`}></Link>)
+            }
+            
             <Outlet></Outlet>
         </div>
     );
